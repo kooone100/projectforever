f05fe8e84553a584d6a2b752304ab36230f803cf
kooone100 <iamphyque@gmail.com>
2026-04-12 22:42:06 +0000

    Settings Updated

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -180,7 +180,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"  # important for Render deployment
 MEDIA_URL = "/media/"
 # MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
