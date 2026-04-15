d56f0eb7b8aaeb38557903c83750979784261c55
kooone100 <iamphyque@gmail.com>
2026-04-15 02:27:13 +0000

    New Env

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -121,8 +121,10 @@ DATABASES = {
 # Render PostgreSQL Database Credentials
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
