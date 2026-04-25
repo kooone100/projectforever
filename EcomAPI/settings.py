324274ecbe21e8c64ab6f1029ff6262940bea475
kooone100 <iamphyque@gmail.com>
2026-04-25 17:50:59 +0000

    Settings Update 3

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -185,7 +185,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"  # important for Render deployment
 # MEDIA_URL = "/media/"
-# MEDIA_ROOT = BASE_DIR / "media"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
